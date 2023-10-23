04f1ec09c08cf7771e7f522c821e751bb8954ef5
JakubGaszczak <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>
2023-10-23 23:56:17 +0200

    Added authMiddleware with JWT token verification and admin role check

---
+++ backend/middleware/authMiddleware.ts
@@ -0,0 +1,48 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "./asyncHandler";
+import { NextFunction, Request, Response } from "express";
+import User from "../models/userModel";
+
+declare module "express" {
+  interface Request {
+    user?: any;
+  }
+}
+
+const protect = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    let token: string;
+
+    token = req.cookies.jwt;
+
+    if (token) {
+      try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+          userId: string;
+        };
+
+        req.user = await User.findById(decoded.userId).select("-password");
+
+        next();
+      } catch (error) {
+        console.log(error);
+        res.status(401);
+        throw new Error("Not authorized, token failed");
+      }
+    } else {
+      res.status(401);
+      throw new Error("Not authorized, no token");
+    }
+  }
+);
+
+const admin = asyncHandler( async(req: Request, res: Response, next: NextFunction) => {
+    if (req.user && req.user.isAdmin) {
+        next()
+    } else {
+        res.status(401)
+        throw new Error("Not authorized as an admin")
+    }
+})
+
+export { protect, admin };
