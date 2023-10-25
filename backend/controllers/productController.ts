1400be4e47cb6e96296c16c358f66a9fd4051397
JakubGaszczak <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>
2023-10-25 11:44:59 +0200

    Created getProductById, createReview, and getTopRated controllers

--- backend/controllers/productController.ts
+++ backend/controllers/productController.ts
@@ -27,59 +27,109 @@ const addProduct = asyncHandler(async (req: Request, res: Response) => {
     category: category,
     weight: weight,
     dimensions: dimensions,
-    materials: materials
-  })
+    materials: materials,
+  });
 
-  await product.save()
+  await product.save();
 
   if (product) {
-    res.status(201).json(product)
+    res.status(201).json(product);
   } else {
-    res.status(500)
-    throw new Error("Failed to create product")
+    res.status(500);
+    throw new Error("Failed to create product");
   }
 });
 
 // @desc    Delete product by ID
 // @route   DELETE /api/products/:id
 // @access  Private/Admin
-const deleteProduct = asyncHandler( async(req: Request, res: Response) => {
-    const product = await Product.findById(req.params.id)
+const deleteProduct = asyncHandler(async (req: Request, res: Response) => {
+  const product = await Product.findById(req.params.id);
 
-    if (product) {
-        await product.deleteOne()
-        res.json({ message: "Product removed" })
-    } else {
-        res.status(404)
-        throw new Error("Product not found")
-    }
-})
+  if (product) {
+    await product.deleteOne();
+    res.json({ message: "Product removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
 
 // @desc    Get all products
 // @route   GET /api/products
 // @access  Public
-const getAllProducts = asyncHandler( async(req: Request, res: Response) => {
-    const products = await Product.find({})
+const getAllProducts = asyncHandler(async (req: Request, res: Response) => {
+  const products = await Product.find({});
 
-    if (products) {
-        res.json(products)
-    } else {
-        res.status(404)
-        throw new Error("Products not found")
-    }
-})
+  if (products) {
+    res.json(products);
+  } else {
+    res.status(404);
+    throw new Error("Products not found");
+  }
+});
 
 // @desc    Get product by ID
 // @route   GET /api/products/:id
 // @access  Public
+const getProductById = asyncHandler(async (req: Request, res: Response) => {
+  const product = await Product.findById(req.params.id);
 
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
 
 // @desc    Create new rewiev
 // @route   POST /api/products/:id/reviews
 // @access  Private
+const createReview = asyncHandler(async (req: Request, res: Response) => {
+  const { comment, rating } = req.body;
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user?._id.toString()
+    );
+
+    if (alreadyReviewed) {
+        res.status(400)
+        throw new Error("Product already reviewed")
+    }
+
+    const review = {
+        user: req.user?._id,
+        name: req.user?.firstName,
+        comment: comment,
+        rating: rating,
+    }
+
+    product.reviews.push(review)
+    product.numReviews = product.reviews.length
+
+    product.rating = product.reviews.reduce((acc: number, item: any) => 
+        item.rating + acc, 0
+    ) / product.reviews.length
+
+    await product.save()
+    res.status(201).json({ message: "Review added" })
+  } else {
+    res.status(404)
+    throw new Error("Product not found")
+  }
+});
 
 // @desc    Get top rated products
 // @route   GET /api/products/top
 // @access  Public
+const getTopRated = asyncHandler( async(req: Request, res: Response) => {
+    const products = await Product.find({}).sort({ rating: -1 }).limit(3)
 
-export { addProduct, deleteProduct, getAllProducts }
+    res.json(products)
+})
+
+
+export { addProduct, deleteProduct, getAllProducts, getProductById, createReview, getTopRated };
--- backend/routes/productRoute.ts
+++ backend/routes/productRoute.ts
@@ -1,10 +1,13 @@
 import express from "express"
-import { addProduct, deleteProduct, getAllProducts } from "../controllers/productController"
+import { addProduct, createReview, deleteProduct, getAllProducts, getProductById, getTopRated } from "../controllers/productController"
 import { protect, admin } from "../middleware/authMiddleware"
 const router = express.Router()
 
 router.post("/", protect, admin, addProduct)
 router.delete("/:id", protect, admin, deleteProduct)
 router.get("/", getAllProducts)
+router.get("/top", getTopRated)
+router.get("/:id", getProductById)
+router.post("/:id/reviews", protect, createReview)
 
 export default router
