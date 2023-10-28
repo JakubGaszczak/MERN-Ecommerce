250bf15ca343164c5c3b4d64d4636c4aa9d22c53
JakubGaszczak <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>
2023-10-28 11:43:33 +0200

    Created Services Component

---
+++ frontend/src/components/Services/Sercives.tsx
@@ -0,0 +1,51 @@
+import { TbTruckDelivery } from "react-icons/tb";
+import { TbAward } from "react-icons/tb";
+import { BiCheckShield } from "react-icons/bi";
+
+const Sercives = () => {
+  return (
+    <section className="container my-5">
+      <div className="row">
+        <div className="col-md-6 col-lg-4 mb-3">
+
+          <div className="card p-3">
+            <h3 className="card-title">
+              <TbTruckDelivery /> Free Delivery
+            </h3>
+            <p className="card-text">
+              Shop with ease thanks to our complimentary delivery service. Enjoy
+              worry-free shopping on all orders
+            </p>
+          </div>
+        </div>
+
+        <div className="col-md-6 col-lg-4 mb-3">
+          <div className="card p-3">
+            <h3 className="card-title">
+              <TbAward /> Quality
+            </h3>
+            <p className="card-text">
+              Exceptional quality is our promise. We take pride in delivering
+              products of the highest standard
+            </p>
+          </div>
+        </div>
+
+        <div className="col-md-6 col-lg-4 mb-3">
+          <div className="card p-3">
+            <h3 className="card-title">
+              <BiCheckShield /> Secure Payment
+            </h3>
+            <p className="card-text">
+              Your payment, our priority. We ensure secure transactions for your
+              peace of mind
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default Sercives;
--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import bannerWatch from "../assets/bannerWatch.jpg"
+import Sercives from '../components/Services/Sercives'
 
 const HomeScreen = () => {
   return (
     <>
         <Banner image={bannerWatch} title="Class and Elegance Combined" buttonText="Shop Now" />
+        <Sercives />
     </>
   )
 }
