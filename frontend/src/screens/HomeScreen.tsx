bcb8286b3db28e75ceb359bd1cacd8ddf21d565b
JakubGaszczak <jakub.gaszczak2000@gmail.com>
2023-11-26 23:59:24 +0100

    Change image file extension to improve compatibility

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import Banner from "../components/Banner";
 import bannerWatch from "../assets/bannerWatch.jpg";
-import bannerBelt from "../assets/bannerBelt.webp";
+import bannerBelt from "../assets/bannerBelt.jpg";
 import Sercives from "../components/Services/Sercives";
 import Products from "../components/Products/Products";
 import TopRated from "../components/TopRated";
